dbf9e6482bec16907f0ffb978f961efd84d0026b
Filip Radovic <f.radovic99@gmail.com>
2024-06-24 12:47:03 +0200

    SignInScreen addition

--- app/src/main/java/com/frcoding/musicplayerapp/screens/sign_in/SignInScreen.kt
+++ app/src/main/java/com/frcoding/musicplayerapp/screens/sign_in/SignInScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,7 +61,8 @@ fun SignInScreen(
             onValueChange = { viewModel.updatePassword(it) },
             label = { Text(text = "Password") },
             singleLine = true,
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Password") }
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Password") },
+            visualTransformation = PasswordVisualTransformation()
         )
 
         Spacer(modifier = Modifier
@@ -69,7 +71,7 @@ fun SignInScreen(
 
         Button(
             onClick = { viewModel.onSignInClick(openAndPopUp) },
-            modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .padding(16.dp, 0.dp)
         ) {
